e820722e80d60ced81665c293e1fa5e669e71381
YesCoops <coopandmgc@gmail.com>
2021-11-16 14:57:48 +0000

    Added code to increment score for right and wrong answers, added Multiply gameType

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,6 +32,8 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -50,8 +52,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right! :D");
+        incrementScore();
     } else {
         alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -69,19 +73,33 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
-    } else {
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
+     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
 
 }
 
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
+
 function incrementScore(){
 
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 }
+
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
 
 function incrementWrongAnswer(){
 
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
@@ -95,8 +113,11 @@ function displaySubtractQuestion(){
     
 }
 
-function displayMultiplyQuestion(){
-    
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
+
 }
 
 function displayDivideQuestion(){
